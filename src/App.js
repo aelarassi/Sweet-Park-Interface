2c79bfa92c30618c8027621d73c3aea4291543a3
ABDELAZIZ EL ARASSI <elarassi.abdelaziz@gmail.com>
2019-08-18 17:29:06 +0100

    add detail infos

--- src/App.js
+++ src/App.js
@@ -1,96 +1,51 @@
 import React, { Component } from "react";
 
+import { _logo } from "./logo";
+
 class App extends Component {
-  state = {
-    resultCard: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      resultCard: false,
+      nomClient: "ABDELAZIZ EL ARASSI",
+      info: "OK",
+      soldeParking: "1732.95",
+      heureFinParking: "2019-08-04 16:31:40",
+      montantAmende: "0.00"
+    };
+  }
+
+  _submit = (e, status) => {
+    e.preventDefault();
+    this.setState({ resultCard: status });
   };
+
   render() {
+    const {
+      nomClient,
+      info,
+      soldeParking,
+      heureFinParking,
+      montantAmende
+    } = this.state;
     return (
       <div className="page">
         <div class="text-center my-5">
-          <img
-            src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
-            class="h-8"
-            alt=""
-          />
+          <img class="h-8" alt="" src={_logo} />
         </div>
         <div className="row mx-0">
           <div>
-            <form className="card mx-auto" action="" method="post">
-              <div className="card-body">
-                {false && <i class="fe fe-ICON_NAME" />}
-                <h3 className="page-title text-color-primary font-weight-normal mb-5 text-center text-uppercase">
-                  Sweet Park Interface 1
-                </h3>
-                <div className="form-group">
-                  <label className="form-label">
-                    Mobile de l'utilisateur :
-                  </label>
-                  <input
-                    className="form-control"
-                    placeholder="Numéro du mobile"
-                    type="tel"
-                    id="mobUser"
-                    name="mobUser"
-                    pattern="[0-9]{9}"
-                    maxlength="10"
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label className="form-label">Recharge Parking :</label>
-                  <div class="input-group">
-                    <input
-                      type="text"
-                      class="form-control"
-                      aria-label="Amount (to the nearest dollar)"
-                      placeholder="Montant en Dhs"
-                      type="tel"
-                      id="RechPark"
-                      name="rechPark"
-                      pattern="[0-9]"
-                      maxlength="5"
-                      required
-                    />
-                    <span class="input-group-append">
-                      <span class="input-group-text">DH</span>
-                    </span>
-                  </div>
-                </div>
-                <div className="form-group">
-                  <label className="form-label">Paiement Amende :</label>
-                  <div class="input-group">
-                    <input
-                      type="text"
-                      class="form-control"
-                      aria-label="Amount (to the nearest dollar)"
-                      placeholder="Montant en Dhs"
-                      type="tel"
-                      id="PaieAmende"
-                      name="paieAmende"
-                      pattern="[0-9]"
-                      maxlength="5"
-                      required
-                    />
-                    <span class="input-group-append">
-                      <span class="input-group-text">DH</span>
-                    </span>
-                  </div>
-                </div>
-                <div className="form-footer">
-                  <button type="submit" className="btn btn-primary btn-block">
-                    Send
-                  </button>
-                </div>
-              </div>
-            </form>
-
-            {this.state.resultCard && (
-              <form className="card mx-auto" action="" method="post">
-                <div className="card-body p-6">
+            {!this.state.resultCard && (
+              <form
+                className="card mx-auto"
+                action=""
+                method="post"
+                // onSubmit={() => this._submit()}
+              >
+                <div className="card-body">
                   {false && <i class="fe fe-ICON_NAME" />}
                   <h3 className="page-title text-color-primary font-weight-normal mb-5 text-center text-uppercase">
-                    Sweet Park Interface 1
+                    Sweet Park Interface
                   </h3>
                   <div className="form-group">
                     <label className="form-label">
@@ -102,9 +57,9 @@ class App extends Component {
                       type="tel"
                       id="mobUser"
                       name="mobUser"
-                      pattern="[0-9]{9}"
+                      pattern="[0-9]{10}"
                       maxlength="10"
-                      required
+                      // required
                     />
                   </div>
                   <div className="form-group">
@@ -114,13 +69,13 @@ class App extends Component {
                         type="text"
                         class="form-control"
                         aria-label="Amount (to the nearest dollar)"
-                        placeholder="Montant en Dhs"
-                        type="tel"
+                        placeholder="Montant en FCFA, FCFA/dhs etc.."
+                        type="number"
                         id="RechPark"
                         name="rechPark"
                         pattern="[0-9]"
                         maxlength="5"
-                        required
+                        // required
                       />
                       <span class="input-group-append">
                         <span class="input-group-text">DH</span>
@@ -134,13 +89,13 @@ class App extends Component {
                         type="text"
                         class="form-control"
                         aria-label="Amount (to the nearest dollar)"
-                        placeholder="Montant en Dhs"
-                        type="tel"
+                        placeholder="Montant en FCFA, FCFA/dhs etc.."
+                        type="number"
                         id="PaieAmende"
                         name="paieAmende"
                         pattern="[0-9]"
                         maxlength="5"
-                        required
+                        // required
                       />
                       <span class="input-group-append">
                         <span class="input-group-text">DH</span>
@@ -148,22 +103,73 @@ class App extends Component {
                     </div>
                   </div>
                   <div className="form-footer">
-                    <button type="submit" className="btn btn-primary btn-block">
+                    <button
+                      type="submit"
+                      className="btn btn-primary btn-block"
+                      onClick={e => this._submit(e, true)}
+                    >
                       Send
                     </button>
                   </div>
                 </div>
               </form>
             )}
+            {this.state.resultCard && (
+              <div className="card mx-auto">
+                <div className="card-body p-6">
+                  {false && <i class="fe fe-ICON_NAME" />}
+                  <h3 className="page-title text-color-primary font-weight-normal mb-5 text-center text-uppercase">
+                    SWEET PARK: DONNEES CLIENT
+                  </h3>
+                  <div className="form-group">
+                    <label className="form-label">Nom du client</label>
+                    <h6 class="ml-2 text-color-primary">{nomClient}</h6>
+                  </div>
+                  <div className="form-group">
+                    <label className="form-label">Info</label>
+                    <h6 class="ml-2 text-color-primary">{info}</h6>
+                  </div>
+
+                  <div className="form-group">
+                    <label className="form-label">
+                      Solde Parking(FCFA/dhs)
+                    </label>
+                    <h6 class="ml-2 text-color-primary">{soldeParking}</h6>
+                  </div>
+                  <div className="form-group">
+                    <label className="form-label">
+                      Heure de Fin de Parking
+                    </label>
+                    <h6 class="ml-2 text-color-primary">{heureFinParking}</h6>
+                  </div>
+                  <div className="form-group">
+                    <label className="form-label">
+                      Montant Amende(FCFA/dhs)
+                    </label>
+                    <h6 class="ml-2 text-color-primary">{montantAmende}</h6>
+                  </div>
+
+                  <div className="form-footer">
+                    <button
+                      type="submit"
+                      className="btn btn-primary btn-block"
+                      onClick={e => this._submit(e, false)}
+                    >
+                      Retour
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
-        <div class="text-center text-muted mb-3">
-          Contact information:
-          <a className="ml-3" href="mailto:contact@qweb-tech.com">
-            contact@qweb-tech.com
-          </a>
-        </div>
-        <div class="text-center text-white pb-5">Qweb Technology</div>
+        <div class="text-center text-white">Qweb Technology</div>
+        <a
+          href="https://github.com/aelarassi"
+          class="text-center text-color-primary pb-5"
+        >
+          aelarassi ©
+        </a>
       </div>
     );
   }
