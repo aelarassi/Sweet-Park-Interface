fbce34cca8974f5d600febce4194f0b5d1ff4fd6
ABDELAZIZ EL ARASSI <elarassi.abdelaziz@gmail.com>
2019-08-19 00:02:12 +0100

    Updates

---
+++ precache-manifest.6b098ea9d67e42ad30b1b5437846a7bd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cebee9eabd9ef1ce3628",
+    "url": "/Sweet-Park-Interface/static/js/runtime~main.cebee9ea.js"
+  },
+  {
+    "revision": "cb589b5a56f1cac64035",
+    "url": "/Sweet-Park-Interface/static/js/main.cb589b5a.chunk.js"
+  },
+  {
+    "revision": "61fb1f8a4dc666426738",
+    "url": "/Sweet-Park-Interface/static/js/1.61fb1f8a.chunk.js"
+  },
+  {
+    "revision": "74be925e5ec964d3a7612970345aca55",
+    "url": "/Sweet-Park-Interface/index.html"
+  }
+];
